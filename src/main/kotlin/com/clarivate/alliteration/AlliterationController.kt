7ee50fc2bb2634069bd3a2f813343cc661478bf6
Danilo Acosta <djav95@hotmail.com>
2020-05-17 17:08:31 +0200

    Initialization

---
+++ src/main/kotlin/com/clarivate/alliteration/AlliterationController.kt
@@ -0,0 +1,10 @@
+package com.clarivate.alliteration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.*
+
+
+@SpringBootApplication
+@RestController
+class AlliterationController {
+}
---
+++ src/test/kotlin/com/clarivate/alliteration/AlliterationControllerTests.kt
@@ -0,0 +1,21 @@
+package com.clarivate.alliteration
+
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AlliterationControllerTests {
+
+	@LocalServerPort
+	private val port = 0
+
+	@Autowired
+	private val restTemplate: TestRestTemplate? = null
+
+}
