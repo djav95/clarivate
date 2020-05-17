5f42caeebe2152d64e768439500d89bf667954cd
Danilo Acosta <djav95@hotmail.com>
2020-05-17 18:55:31 +0200

    Alliteration controller done and tested

--- src/main/kotlin/com/clarivate/alliteration/AlliterationController.kt
+++ src/main/kotlin/com/clarivate/alliteration/AlliterationController.kt
@@ -1,10 +1,53 @@
 package com.clarivate.alliteration
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-
+/**
+ * Spring Boot Controller for alliterations.
+ *
+ * @author Danilo Acosta
+ */
 @SpringBootApplication
 @RestController
 class AlliterationController {
+
+    /**
+     * Get the highest alliteration of a sentence.
+     *
+     * @param sentence Any [String] sentence (including an empty sentence) with any character.
+     * @return A string containing the result: "Alliteration percentage, [Char], [Float]". Where [Char]
+     * is the consonant with highest alliteration percentage, and [Float] is the percentage with two
+     * decimal precision. Or "Sentence was not provided" if the sentence is null of empty
+     */
+    @GetMapping("/alliteration")
+    fun alliteration(@RequestParam(value = "sentence") sentence: String): String {
+        if (!sentence.isNullOrEmpty()) {
+            val alliterationCounter: HashMap<String, Int> = HashMap()
+            // Split a sentence by whitespaces
+            val tokens = sentence.split("\\s".toRegex())
+
+            // For each token (or word), checks if the first character is a consonant, and adds the number of occurrence
+            tokens.forEach { token ->
+                val ch: Char = token.first().toLowerCase()
+                if (ch.isLetter() && (ch != 'a' || ch != 'e' || ch != 'i' || ch != 'o' || ch != 'u')) {
+                    val counterValue: Int? = alliterationCounter[ch.toString()]
+                    alliterationCounter[ch.toString()] = if (counterValue != null) counterValue + 1 else 1
+                }
+            }
+
+            //Gets the consonant with most occurrences in the sentence
+            val maxBy: Map.Entry<String, Int>? = alliterationCounter.maxBy { it.value }
+            val alliteration = maxBy?.key
+            val percentage: Float? = if (maxBy != null) (maxBy.value.toFloat() / tokens.size.toFloat()) * 100 else 0F
+
+            //Returns the
+            return String.format("Alliteration percentage, %s, %.2f", alliteration, percentage)
+        } else {
+            return String.format("Sentence was not provided")
+        }
+    }
 }
--- src/test/kotlin/com/clarivate/alliteration/AlliterationControllerTests.kt
+++ src/test/kotlin/com/clarivate/alliteration/AlliterationControllerTests.kt
@@ -18,4 +18,44 @@ class AlliterationControllerTests {
 	@Autowired
 	private val restTemplate: TestRestTemplate? = null
 
+	@Test
+	@Throws(Exception::class)
+	fun testExample() {
+		val sentence = "Becky's beagle barked and bayed, becoming bothersome for Billy."
+		assertThat(restTemplate!!.getForObject("http://localhost:$port/alliteration?sentence=$sentence",
+				String::class.java)).contains("Alliteration percentage, b, 77.78")
+	}
+
+	@Test
+	@Throws(Exception::class)
+	fun testText1() {
+		val sentence = "Mike made mellow music with his new microphone."
+		assertThat(restTemplate!!.getForObject("http://localhost:$port/alliteration?sentence=$sentence",
+				String::class.java)).contains("Alliteration percentage, m, 62.50")
+	}
+
+	@Test
+	@Throws(Exception::class)
+	fun testText2() {
+		val sentence = "Yarvis yanked his ankle at yoga, and Yolanda yelled out in surprise."
+		assertThat(restTemplate!!.getForObject("http://localhost:$port/alliteration?sentence=$sentence",
+				String::class.java)).contains("Alliteration percentage, y, 41.67")
+	}
+
+	@Test
+	@Throws(Exception::class)
+	fun emptySentence() {
+		val sentence = ""
+		assertThat(restTemplate!!.getForObject("http://localhost:$port/alliteration?sentence=$sentence",
+				String::class.java)).contains("Sentence was not provided")
+	}
+
+	@Test
+	@Throws(Exception::class)
+	fun invalidChar() {
+		val sentence = "!No .result."
+		assertThat(restTemplate!!.getForObject("http://localhost:$port/alliteration?sentence=$sentence",
+				String::class.java)).contains("Alliteration percentage, null, 0.00")
+	}
+
 }
